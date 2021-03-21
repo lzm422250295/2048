3eb7b72d1a29eb784b8fd8b5ebb802000a44d459
lzm422250295 <422250295@qq.com>
2021-03-21 23:52:29 +0800

    更新

--- 2048/Program.cs
+++ 2048/Program.cs
@@ -24,97 +24,167 @@ namespace _2048
 
         static void Main(string[] args)
         {
-            //去零
-            int[] delZero(int[] arr)
-            {
-                int lefIndex = 0; //左下标
-                int rigIndex = arr.Length - 1; //右下标
-                int[] newArr = new int[arr.Length];
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    if (arr[i] == 0) newArr[rigIndex--] = 0;
-                    else newArr[lefIndex++] = arr[i];
-                }
-                return newArr;
-            }
-
-            //合并
-            int[] add(ref bool canAdd, int[] newArr)
-            {
-                int length = newArr.Length;
-                for (int i = 0; i < length; i++)
-                {
-                    if (i == length - 1) continue;
-                    if (newArr[i] == newArr[i + 1] && canAdd == true)
-                    {
-                        newArr[i] += newArr[i + 1];
-                        newArr[i + 1] = 0;
-                        canAdd = false;
-                    }
-                }
+            Fun2048 Fun2048 = new Fun2048();
 
 
 
-                return newArr;
-            }
-
-            //获取对应列的一维数组
-            int[] getCol(int index, int[,] map)
-            {
-                int[] arr = new int[map.GetLength(0)];
-                for (int i = 0; i < map.GetLength(0); i++)
-                {
-                    arr[i] = map[i, index];
-                }
-                return arr;
-            }
-
-            //处理一维数组
-            int[] getArr(int[] arr)
-            {
-                bool canAdd = true; //合并一次
-                int[] array = delZero(add(ref canAdd, delZero(arr)));
-                //Array.Reverse(array);
-
-                //合并
-                return array;
-            }
 
 
-            int[,] map = new int[4, 4] {
-                {2,0,4,16},
-                {2,4,0,16},
-                {4,4,2,0},
-                {8,4,2,16},
+            int[,] map = new int[4, 6] {
+                {2,0,4,16,2,4},
+                {2,4,0,16,2,2},
+                {4,4,2,0,2,2},
+                {8,4,2,16,2,4},
             };
 
-            foreach (int item in getArr(getCol(3, map)))
-            {
-                Console.WriteLine(item);
-            }
-
-            Console.WriteLine();
-            for (int c = 0; c < map.GetLength(1); c++)
-            {
-                for (int r = 0; r < map.GetLength(0); r++)
-                {
-                    //Console.WriteLine(map[r, c]);
-                    Console.WriteLine(getCol(c, map)[r]);
-
-                }
-            }
-
-            //for (int r = 0; r < map.GetLength(0); r++)
+            Fun2048.MoveDown(map);
+            //foreach (int item in getArr(getCol(3, map)))
             //{
-            //    for (int c = 0; c < map.GetLength(1); c++)
-            //    {
-            //        Console.Write(map[r, c] + "\t");
-            //    }
-            //    Console.WriteLine();
+            //    Console.WriteLine(item);
             //}
+
+            //Console.WriteLine();
+            //for (int c = 0; c < map.GetLength(1); c++)
+            //{
+            //    int[] arr = getArr(getCol(c, map));
+            //    for (int i = 0; i < arr.GetLength(0); i++)
+            //    {
+            //        map[i,c] = arr[i];
+            //    }
+            //}
+
+            for (int r = 0; r < map.GetLength(0); r++)
+            {
+                for (int c = 0; c < map.GetLength(1); c++)
+                {
+                    Console.Write(map[r, c] + "\t");
+                }
+                Console.WriteLine();
+            }
 
         }
 
 
+    }
+    class Fun2048
+    {
+        //去零到右
+        private int[] delZeroToRig(int[] arr)
+        {
+            int lefIndex = 0; //左下标
+            int rigIndex = arr.Length - 1; //右下标
+            int[] newArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0) newArr[rigIndex--] = 0;
+                else newArr[lefIndex++] = arr[i];
+            }
+            return newArr;
+        }
+        //去零到左
+        private int[] delZeroToLef(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            int lefIndex = 0; //左下标
+            int rigIndex = arr.Length - 1; //右下标
+            int[] newArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0) newArr[rigIndex--] = arr[i];
+                else newArr[lefIndex++] = 0;
+            }
+            return newArr;
+        }
+
+        //向左合并
+        private int[] addLef(ref bool canAdd, int[] newArr)
+        {
+            int length = newArr.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (i == length - 1) continue;
+                if (newArr[i] == newArr[i + 1] && canAdd == true)
+                {
+                    newArr[i] += newArr[i + 1];
+                    newArr[i + 1] = 0;
+                    canAdd = false;
+                }
+            }
+            return newArr;
+        }
+
+        //向右合并
+        private int[] addRig(ref bool canAdd, int[] newArr)
+        {
+            int length = newArr.Length;
+            for (int i = (length-1); i > 0; i--)
+            {
+                if (newArr[i] == newArr[i - 1] && canAdd == true)
+                {
+                    newArr[i] += newArr[i - 1];
+                    newArr[i - 1] = 0;
+                    canAdd = false;
+                }
+            }
+            return newArr;
+        }
+        //获取对应列的一维数组
+        private int[] getCol(int c, int[,] map)
+        {
+            int[] arr = new int[map.GetLength(0)];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                arr[i] = map[i, c];
+            }
+            return arr;
+        }
+
+        //获取对应行的一维数组
+        private int[] getRow(int r, int[,] map)
+        {
+            int[] arr = new int[map.GetLength(1)];
+            for (int i = 0; i < map.GetLength(1); i++)
+            {
+                arr[i] = map[r, i];
+            }
+            return arr;
+        }
+
+        //处理一维数组
+        private int[] getArr(int[] arr,bool isUpRig)
+        {
+            bool canAdd = true; //合并一次
+            int[] array = isUpRig
+                ?delZeroToRig(addLef(ref canAdd, delZeroToRig(arr)))
+                : delZeroToLef(addRig(ref canAdd, delZeroToLef(arr)));
+            return array;
+        }
+
+        public void MoveUp(int[,] array)
+        {
+            for (int c = 0; c < array.GetLength(1); c++)
+            {
+                int[] arr = getArr(getCol(c, array),true);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    array[i, c] = arr[i];
+                }
+            }
+        }
+
+        public void MoveDown(int[,] array)
+        {
+            for (int c = 0; c < array.GetLength(1); c++)
+            {
+                int[] arr = getArr(getCol(c, array), false);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    array[i, c] = arr[i];
+                }
+            }
+        }
     }
 }
